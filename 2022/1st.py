7163272cc42857e08aae9d82ab3d8afc4d9b02d6
BSOD2528 <bsod2528@protonmail.com>
2022-12-02 19:09:04 +0530

    Day 2: Done

--- 2022/1st.py
+++ 2022/1st.py
@@ -8,7 +8,7 @@ white: str = f"{escape}[0;1;37m"
 the_arrow: str = f"{escape}[0;1;37;40m > "
 
 # All hail Zeus432 for this part :worship:
-with open("2022\\elves.txt") as file:
+with open("2022\\elf_calorie.txt") as file:
     elf_dict: Dict[int,
                    List[Any]] = {i + 1: [int(f) for f in x.split("\n")] for i,
                                  x in enumerate(file.read().strip().split("\n\n"))}
@@ -20,6 +20,9 @@ for calories in elf_dict.values():
 
 calorie_list.sort()
 highest_calorie: int = calorie_list[-1]
+
+second_highest: int = calorie_list[-2]
+third_highest: int = calorie_list[-3]
 
 system("cls")
 
@@ -37,3 +40,12 @@ for key, value in elf_dict.items():
     if sum(value) == highest_calorie:
         print(
             f"{the_arrow}{white} Therefore, the Elves should go and ask {blue}{key}{suffix} Elf{escape}[0m{white} as they have the highest calorie of: {red}{highest_calorie} {escape}[0m")
+    if sum(value) == second_highest:
+        print(
+            f"{the_arrow}{white} Therefore, the Elves should go and ask {blue}{key}{suffix} Elf{escape}[0m{white} as they have the highest calorie of: {red}{second_highest} {escape}[0m")
+    if sum(value) == third_highest:
+        print(
+            f"{the_arrow}{white} Therefore, the Elves should go and ask {blue}{key}{suffix} Elf{escape}[0m{white} as they have the highest calorie of: {red}{third_highest} {escape}[0m")
+
+print(f"{the_arrow}{white} And the total calories by those three is {red}{highest_calorie + second_highest + third_highest}{escape}[0m")
+
---
+++ 2022/2nd.py
@@ -0,0 +1,45 @@
+from typing import Dict, List
+
+escape: str = "\x1b"
+red: str = f"{escape}[0;1;31m"
+blue: str = f"{escape}[0;1;34m"
+white: str = f"{escape}[0;1;37m"
+the_arrow: str = f"{escape}[0;1;37;40m > "
+
+with open("2022\\rps.txt") as file:
+    strategy_list: List[str] = [x for x in file.read().strip().split("\n")]
+
+# Part 1
+partial_outcomes: Dict[str, int] = {
+    "A X": 4,
+    "A Y": 8,
+    "A Z": 3,
+    "B X": 1,
+    "B Y": 5,
+    "B Z": 9,
+    "C X": 7,
+    "C Y": 2,
+    "C Z": 6}
+
+total_score: int = 0
+for game in strategy_list:
+    total_score += partial_outcomes[game]
+print(f"{the_arrow}{white} Therefore, we will be getting a total of {red}{total_score}{white} considering the fact we are {blue}assuming{white} the strategy.{escape}[0m")
+
+
+# Part 2
+actual_outcomes: Dict[str, int] = {
+    "A X": 3,
+    "A Y": 4,
+    "A Z": 8,
+    "B X": 1,
+    "B Y": 5,
+    "B Z": 9,
+    "C X": 2,
+    "C Y": 6,
+    "C Z": 7}
+
+total_score: int = 0
+for game in strategy_list:
+    total_score += actual_outcomes[game]
+print(f"{the_arrow}{white} Therefore, we will be getting a total of {red}{total_score}{white} considering the fact we are going as {blue}per the accurate{white} strategy.{escape}[0m")
