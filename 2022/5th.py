430658026835971b9b4079b9eb7c47059cca08ef
BSOD2528 <bsod2528@protonmail.com>
2022-12-07 11:29:01 +0530

    Format changes

--- 2022/5th.py
+++ 2022/5th.py
@@ -8,28 +8,40 @@ from file_formats import the_arrow, white, blue, red, escape
 cratemover_9000: DefaultDict = defaultdict(list)
 
 for line in open("2022\\crates.txt"):
-	if "[" in line:
-		for char in range(1, len(line) - 1, 4):
-			if line[char] != " ":
-				cratemover_9000[(char - 1) // 4].append(line[char])
-	elif line.startswith("move"):
-		alpha, beta, gamma = map(int, re.findall(r"\d+", line))
-		cratemover_9000[gamma - 1] = cratemover_9000[beta - 1][:alpha][::-1] + cratemover_9000[gamma - 1]
-		cratemover_9000[beta - 1] = cratemover_9000[beta - 1][alpha:]
+    if "[" in line:
+        for char in range(1, len(line) - 1, 4):
+            if line[char] != " ":
+                cratemover_9000[(char - 1) // 4].append(line[char])
+    elif line.startswith("move"):
+        alpha, beta, gamma = map(int, re.findall(r"\d+", line))
+        cratemover_9000[gamma - 1] = cratemover_9000[beta - \
+            1][:alpha][::-1] + cratemover_9000[gamma - 1]
+        cratemover_9000[beta - 1] = cratemover_9000[beta - 1][alpha:]
 
-print(f"{the_arrow}{white} Therefore after rearranging using the {blue}Cratemover 9000{white} the order of crates which are at the top are: {red}" + "".join(cratemover_9000[i][0] for i in range(len(cratemover_9000))) + f"{escape}[0m")
+print(
+    f"{the_arrow}{white} Therefore after rearranging using the {blue}Cratemover 9000{white} the order of crates which are at the top are: {red}" +
+    "".join(
+        cratemover_9000[i][0] for i in range(
+            len(cratemover_9000))) +
+    f"{escape}[0m")
 
 
 cratemover_9001: DefaultDict = defaultdict(list)
 
 for line in open("2022\\crates.txt"):
-	if "[" in line:
-		for i in range(1, len(line) - 1, 4):
-			if line[i] != " ":
-				cratemover_9001[(i - 1) // 4].append(line[i])
-	elif line.startswith("move"):
-		delta, epsilon, zeta = map(int, re.findall(r"\d+", line))
-		cratemover_9001[zeta - 1] = cratemover_9001[epsilon - 1][:delta] + cratemover_9001[zeta - 1]
-		cratemover_9001[epsilon - 1] = cratemover_9001[epsilon - 1][delta:]
+    if "[" in line:
+        for i in range(1, len(line) - 1, 4):
+            if line[i] != " ":
+                cratemover_9001[(i - 1) // 4].append(line[i])
+    elif line.startswith("move"):
+        delta, epsilon, zeta = map(int, re.findall(r"\d+", line))
+        cratemover_9001[zeta - 1] = cratemover_9001[epsilon - \
+            1][:delta] + cratemover_9001[zeta - 1]
+        cratemover_9001[epsilon - 1] = cratemover_9001[epsilon - 1][delta:]
 
-print(f"{the_arrow}{white} Therefore after rearranging using the {blue}Cratemover 9001{white} the order of crates which are at the top are: {red}" + "".join(cratemover_9001[i][0] for i in range(len(cratemover_9001))) + f"{escape}[0m")
+print(
+    f"{the_arrow}{white} Therefore after rearranging using the {blue}Cratemover 9001{white} the order of crates which are at the top are: {red}" +
+    "".join(
+        cratemover_9001[i][0] for i in range(
+            len(cratemover_9001))) +
+    f"{escape}[0m")
--- 2022/6th.py
+++ 2022/6th.py
@@ -4,6 +4,7 @@ from file_formats import the_arrow, white, blue, red, escape
 with open("2022\\datastream.txt") as file:
     datatream: str = file.readline()
 
+
 def get_index(data: str, length: int) -> int:
     index: int = 0
     for alpha in range(0, len(data) - 1):
@@ -13,11 +14,14 @@ def get_index(data: str, length: int) -> int:
             break
     return index
 
+
 def start_of_package():
     return get_index(datatream, 4)
 
+
 def start_of_message():
     return get_index(datatream, 14)
+
 
 print(
     f"{the_arrow}{white} Therefore, the number of characters to be processed {blue}before the start of the first start-of-package marker is detected{white} is: {red}{start_of_package()}{escape}[0m")
